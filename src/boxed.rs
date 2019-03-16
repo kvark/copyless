54f3a60f0cf5140184c114e5521b4b2649a55ec3
Dzmitry Malyshau <kvarkus@gmail.com>
2019-03-15 23:07:19 -0400

    Add box helpers

---
+++ src/boxed.rs
@@ -0,0 +1,45 @@
+use std::{alloc, mem, ptr};
+
+
+const ALIGNMENT: usize = 4; //TODO
+
+/// A typesafe helper that stores the allocated pointer without the data initialized.
+pub struct BoxAllocation<T>(*mut T);
+
+impl<T> BoxAllocation<T> {
+    /// Consumes self and writes the given value into the allocation.
+    pub fn init(mut self, value: T) -> Box<T> {
+        let ptr = mem::replace(&mut self.0, ptr::null_mut());
+        unsafe {
+            ptr::write(ptr, value);
+            Box::from_raw(ptr)
+        }
+    }
+}
+
+impl<T> Drop for BoxAllocation<T> {
+    fn drop(&mut self) {
+        if !self.0.is_null() {
+            let layout = alloc::Layout::from_size_align(mem::size_of::<T>(), ALIGNMENT).unwrap();
+            unsafe {
+                alloc::dealloc(self.0 as *mut u8, layout);
+            }
+        }
+    }
+}
+
+
+/// Helper trait for a `Box` type that allocates up-front.
+pub trait BoxHelper<T> {
+    /// Allocates the storage without providing any data.
+    fn alloc() -> BoxAllocation<T>;
+}
+
+impl<T> BoxHelper<T> for Box<T> {
+    fn alloc() -> BoxAllocation<T> {
+        let layout = alloc::Layout::from_size_align(mem::size_of::<T>(), ALIGNMENT).unwrap();
+        BoxAllocation(unsafe {
+            alloc::alloc(layout) as *mut T
+        })
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 
 //! Helper extensions of standard containers that allow memcopy-less operation.
 
+mod boxed;
 mod vec;
 
+pub use boxed::{BoxAllocation, BoxHelper};
 pub use vec::{VecAllocation, VecEntry, VecHelper};
